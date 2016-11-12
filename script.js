5fbca6569e141bb6e983c7ecf7ce9aede60eb645
Katharine Jiang <katharine.jiang9@gmail.com>
2016-11-12 15:05:37 -0800

    merged all features

--- script.js
+++ script.js
@@ -113,3 +113,137 @@ function newElement() {
   close_helper(li);
 }
 // end Todo List Stuff
+
+/**
+ * @param {string} searchTerm - Search term for Google Image search.
+ * @param {function(string,number,number)} callback - Called when an image has
+ *   been found. The callback gets the URL, width and height of the image.
+ * @param {function(string)} errorCallback - Called when the image is not found.
+ *   The callback gets a string that describes the failure reason.
+ */
+function getImageUrl(searchTerm, callback, errorCallback) {
+  // Google image search - 100 searches per day.
+  // https://developers.google.com/image-search/
+  var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
+    '?v=1.0&q=' + encodeURIComponent(searchTerm);
+  var x = new XMLHttpRequest();
+  x.open('GET', searchUrl);
+  // The Google image search API responds with JSON, so let Chrome parse it.
+  x.responseType = 'json';
+  x.onload = function() {
+    // Parse and process the response from Google Image Search.
+    var response = x.response;
+    if (!response || !response.responseData || !response.responseData.results ||
+        response.responseData.results.length === 0) {
+      errorCallback('No response from Google Image search!');
+      return;
+    }
+    var firstResult = response.responseData.results[0];
+    // Take the thumbnail instead of the full image to get an approximately
+    // consistent image size.
+    var imageUrl = firstResult.tbUrl;
+    var width = parseInt(firstResult.tbWidth);
+    var height = parseInt(firstResult.tbHeight);
+    console.assert(
+        typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
+        'Unexpected respose from the Google Image Search API!');
+    callback(imageUrl, width, height);
+  };
+  x.onerror = function() {
+    errorCallback('Network error.');
+  };
+  x.send();
+}
+
+function renderStatus(statusText) {
+  // document.getElementById('status').textContent = statusText;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  //getWeather()
+  var url = "google.com"
+  getImageUrl(url, function(imageUrl, width, height) {
+    renderStatus('Search term: ' + url + '\n' +
+          'Google image search result: ' + imageUrl);
+    var imageResult = document.getElementById('image-result');
+      // Explicitly set the width/height to minimize the number of reflows. For
+      // a single image, this does not matter, but if you're going to embed
+      // multiple external images in your page, then the absence of width/height
+      // attributes causes the popup to resize multiple times.
+    imageResult.width = width;
+    imageResult.height = height;
+    imageResult.src = imageUrl;
+    imageResult.hidden = false;
+  }, function(errorMessage) {
+      console.log("message")
+    renderStatus('Cannot display image. ' + errorMessage);
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function(){ 
+    var xmlhttp = new XMLHttpRequest();
+    var url = "http://api.wunderground.com/api/0233c96d7f0837b9/conditions/q/CA/San_Francisco.json";
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var myArr = JSON.parse(this.responseText);
+            myFunction(myArr);
+        } 
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+
+    function myFunction(arr) {
+        var out = "";
+        console.log(arr['current_observation']["display_location"]["city"]);
+        out += arr['current_observation']["display_location"]["city"];  
+        out += "\n" 
+        out += arr['current_observation']["weather"];  
+        out += "\n";
+        out += arr['current_observation']["temperature_string"];                                        
+        document.getElementById("weather").innerHTML = out;
+        document.getElementById("weatherIMG").src = arr["current_observation"]["icon_url"];
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function(){ 
+    var xmlhttp = new XMLHttpRequest();
+    var url = "https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey=f18591ef19a34f3eb023911fbebffa16";
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var myArr = JSON.parse(this.responseText);
+            // console.log(myArr);
+            myFunction(myArr);
+        } 
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+
+    function myFunction(arr) {
+        var i;
+        for (i = 0; i < 3; i++) {
+            var a = document.createElement('a');
+            var br = document.createElement("br");
+            var linkText = document.createTextNode(arr["articles"][i]["title"]);
+            a.appendChild(linkText);
+            a.appendChild(br);
+            a.title = arr["articles"][i]["title"];
+            a.href = arr["articles"][i]["url"];
+            document.body.appendChild(a);   
+        }
+        // document.getElementById("newsIMG").src = arr["articles"][i]["urlToImage"];
+    }
+});
+
+document.querySelector('#go-to-options').addEventListener(function() {
+  if (chrome.runtime.openOptionsPage) {
+    // New way to open options pages, if supported (Chrome 42+).
+    chrome.runtime.openOptionsPage();
+  } else {
+    // Reasonable fallback.
+    window.open(chrome.runtime.getURL('options.html'));
+  }
+});
+
+// f18591ef19a34f3eb023911fbebffa16
+
+
